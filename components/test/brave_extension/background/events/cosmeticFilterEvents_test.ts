8a8b297edc5397e0de89da428f9c922e7896b27c
Snuupy <blee@brave.com>
2019-05-24 13:07:30 -0700

    tests to be filled out

---
+++ components/test/brave_extension/background/events/cosmeticFilterEvents_test.ts
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import '../../../../brave_extension/extension/brave_extension/background/events/cosmeticFilterEvents'
+import cosmeticFilterActions from '../../../../brave_extension/extension/brave_extension/background/actions/cosmeticFilterActions'
+import { ChromeEvent } from '../../../testData'
+
+describe('context menu tests', () => {
+  it('unique selector library ONLY called when \'Block Element\' selected') => {
+  }
+
+  it('unique selector library not called when context menu opened', () => {
+  })
+
+  it('window.prompt called on right click', () => {
+    
+  })
+})
